cb7abb6a644be1139cb41f3d4025d89eae24dac6
Gary <Garyshemyakin@gmail.com>
2015-10-21 06:35:38 +0300

    added folder tracking

--- Lotsa_dicts/Lotsa_dicts.cpp
+++ Lotsa_dicts/Lotsa_dicts.cpp
@@ -15,7 +15,7 @@ struct s_params
 	string inputDir;
 	string outputDir;
 	HANDLE stopEvent;
-	HANDLE dirChangedEvent;
+	//HANDLE dirChangedEvent;
 };
 void chop_file(string filename, string inputDir, string outputDir);
 
@@ -30,7 +30,7 @@ DWORD WINAPI make_dicts(void* params) //отслеживает папку и рубит её на словарик
 		WaitForSingleObject(parameters->stopEvent, 0) != WAIT_OBJECT_0)
 		//WaitForSingleObject в этом случае проверяет,
 		//находится ли ивент в сигнальном состоянии
-		//WaitForMultipleObject для нескольких ивентов
+		//WaitForMultipleObjects для нескольких ивентов
 		//FindNextChangeNotification для получения изменения папки
 	{
 		string filename = fileInfo.cFileName;
@@ -39,18 +39,53 @@ DWORD WINAPI make_dicts(void* params) //отслеживает папку и рубит её на словарик
 			break;
 	}
 	FindClose(fileSearch);
+
+	HANDLE dirChangedEvent = FindFirstChangeNotification(
+		parameters->inputDir.c_str(),
+		false,
+		FILE_NOTIFY_CHANGE_FILE_NAME);
+
+	HANDLE events[2];
+	events[0] = parameters->stopEvent;
+	events[1] = dirChangedEvent;
+
+	while (WaitForMultipleObjects(2, events, false, INFINITE) != 0)
+	{
+		
+		fileSearch = FindFirstFile(filenamePattern.c_str(), &fileInfo);
+		if (fileSearch != INVALID_HANDLE_VALUE)
+			//&& WaitForSingleObject(parameters->stopEvent, 0) != WAIT_OBJECT_0)
+			//не работает - ивент выхода активируется с пометкой таймаута, но WaitForMultipleObjects выходит из программы
+		{
+			string filename = fileInfo.cFileName;
+			chop_file(filename, parameters->inputDir, parameters->outputDir);
+			if (FindNextFile(fileSearch, &fileInfo) == false)
+				break;
+		} 
+		FindClose(fileSearch);
+		FindNextChangeNotification(dirChangedEvent); //видимо, переходит на следующее по списку изменение папки
+		//работает очень странно, но без него всё уходит в бесконечный цикл =(
+	}
+	//сейчас работает с некоторым оверхедом из-за создания хендла fileSearch каждый раз
+	//но лучше, чем ничего!
+	//спросить, как улучшить
+
+	CloseHandle(dirChangedEvent);
+
+	//FindClose(fileSearch);
 	return 0;
 }
 
 void chop_file(string filename, string inputDir, string outputDir) //Buy for 4.99$ - SALE!!111oneone
 {
-	cout << "MWAHAHAHAHAHA" << endl;
-	HANDLE inputFile = CreateFile((inputDir+filename).c_str(), GENERIC_READ, 0,
+	cout << "chopping file " << filename << endl;
+	string inputFileName = inputDir + filename;
+	HANDLE inputFile = CreateFile(inputFileName.c_str(), GENERIC_READ, 0,
 		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	HANDLE outputFile = CreateFile((outputDir+"out_"+filename).c_str(), GENERIC_WRITE, 0,
 		NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	std::map<std::string, int> wordCount;
-	std::string delims = (char*)" ,.\n\r:\\\"()='-/;";
+	map<string, int> wordCount;
+	string delims = " ,.\n\r:\\\"()='-/;";
 	int sizeOfBuffer = 255;
 	char* buffer = new char[sizeOfBuffer + 1];
 	DWORD bytesRead;
@@ -80,7 +115,7 @@ void chop_file(string filename, string inputDir, string outputDir) //Buy for 4.9
 				strcpy(next, temp);
 			while (temp != NULL)
 			{
-				wordCount[std::string(current)]++;
+				wordCount[string(current)]++;
 				strcpy(current, next);
 				temp = strtok(NULL, delims.c_str());
 				if (temp != NULL)
@@ -95,7 +130,7 @@ void chop_file(string filename, string inputDir, string outputDir) //Buy for 4.9
 	for (auto x : wordCount)
 	{
 		std::string dictionaryLine = x.first + " - " + to_string(x.second) + "\r\n";
-		std::cout << dictionaryLine;
+		//std::cout << dictionaryLine;
 		DWORD bytesWritten;
 		WriteFile(outputFile, dictionaryLine.c_str(), strlen(dictionaryLine.c_str()), &bytesWritten, NULL);
 	}
@@ -103,27 +138,37 @@ void chop_file(string filename, string inputDir, string outputDir) //Buy for 4.9
 
 	CloseHandle(inputFile);
 	CloseHandle(outputFile);
+	DeleteFile(inputFileName.c_str());
+}
 
+void add_trailing_slash(string &s)
+{
+	if (s[s.length() - 1] != '\\')
+		s += "\\";
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "Входная папка:" << endl;
+	setlocale(LC_ALL, "Russian");
 	string inputDir,outputDir;
+	cout << "Входная папка:" << endl;
 	cin >> inputDir;
 	cout << "Выходная папка:" << endl;
 	cin >> outputDir;
-	HANDLE stopEvent = CreateEvent(NULL, false, false, NULL);
-	HANDLE dirChangedEvent = FindFirstChangeNotification(
-						inputDir.c_str(), 
-						false, 
-						FILE_NOTIFY_CHANGE_FILE_NAME);
+	cin.ignore();
+	//inputDir = "D:\\test\\in\\";
+	//outputDir = "D:\\test\\out\\";
+	//////////////////////////////////////
+	add_trailing_slash(inputDir);
+	add_trailing_slash(outputDir);
+	//////////////////////////////////////
+	HANDLE stopEvent = CreateEvent(NULL, true, false, NULL);
 	//////////////////////////////////////////////////
 	s_params* threadParameters = new s_params;
 	threadParameters->inputDir = inputDir;
 	threadParameters->outputDir = outputDir;
 	threadParameters->stopEvent = stopEvent;
-	threadParameters->dirChangedEvent = dirChangedEvent;
+	//threadParameters->dirChangedEvent = dirChangedEvent;
 	//////////////////////////////////////////////////
 	DWORD threadID; //ИД потока
 	HANDLE dictThread = CreateThread(
@@ -133,11 +178,19 @@ int _tmain(int argc, _TCHAR* argv[])
 						threadParameters, 
 						0, 
 						&threadID);
-	//TODO завершение программы энтером с помощью ивента stopEvent
+
+	while (WaitForSingleObject(stopEvent, 0) != WAIT_OBJECT_0)
+	{
+		char c = cin.get();
+		if (c == '\n')
+			SetEvent(stopEvent);
+	}
+
 	WaitForSingleObject(dictThread, INFINITE); //ждём, пока завершится второй поток
+
 	CloseHandle(dictThread);
 	CloseHandle(stopEvent);
-	CloseHandle(dirChangedEvent);
+	//CloseHandle(dirChangedEvent);
 	return 0;
 }
 
